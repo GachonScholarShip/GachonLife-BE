f425b64ed350791f5b165d754ae687e350beadc4
koreaioi <koreaioi@gachon.ac.kr>
2025-03-28 22:50:42 +0900

    build: 초기 테스트 빌드

---
+++ User-Service/src/test/java/com/gachonproject/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachonproject.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
