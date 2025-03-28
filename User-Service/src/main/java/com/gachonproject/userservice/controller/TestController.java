4f68e83e08bf8f6b421f7b9691a09b04bc279bd6
koreaioi <koreaioi@gachon.ac.kr>
2025-03-28 23:53:32 +0900

    test: CI.yml 테스트 6

--- User-Service/src/main/java/com/gachonproject/userservice/controller/TestController.java
+++ User-Service/src/main/java/com/gachonproject/userservice/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test6")
-    public String test6() {
-        return "test6";
+    @GetMapping("/test7")
+    public String test7() {
+        return "test7";
     }
 }
