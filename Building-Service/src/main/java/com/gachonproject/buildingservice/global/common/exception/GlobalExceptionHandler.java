71c8e105e900d2d13ffd6438393309420a0fdf63
koreaioi <koreaioi@gachon.ac.kr>
2025-04-17 16:35:03 +0900

    fix: Building, Movement import 경로 수정

--- Building-Service/src/main/java/com/gachonproject/buildingservice/global/common/exception/GlobalExceptionHandler.java
+++ Building-Service/src/main/java/com/gachonproject/buildingservice/global/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.gachonproject.buildingservice.global.common.exception;
 
-import com.gachonproject.userservice.global.common.exception.BaseException;
-import com.gachonproject.userservice.global.common.response.ApiResponse;
+import com.gachonproject.buildingservice.global.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
