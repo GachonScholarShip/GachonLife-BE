a7ec8529b12884bbc0b1f5cc23053448839a3598
koreaioi <koreaioi@gachon.ac.kr>
2025-04-22 20:59:45 +0900

    feat: Role, Sex 열거형 추가

---
+++ User-Service/src/main/java/com/gachonproject/userservice/global/common/entity/enums/Role.java
@@ -0,0 +1,5 @@
+package com.gachonproject.userservice.global.common.entity.enums;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ User-Service/src/main/java/com/gachonproject/userservice/global/common/entity/enums/Sex.java
@@ -0,0 +1,5 @@
+package com.gachonproject.userservice.global.common.entity.enums;
+
+public enum Sex {
+    MALE, FEMALE
+}
