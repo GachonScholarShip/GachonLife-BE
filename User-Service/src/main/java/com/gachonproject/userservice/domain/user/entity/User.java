2afcf71b551fa8acd1dc030d2ba3c584bfaf1fb9
koreaioi <koreaioi@gachon.ac.kr>
2025-04-22 21:44:58 +0900

    feat: @Column과 @Enumerated 추가

--- User-Service/src/main/java/com/gachonproject/userservice/domain/user/entity/User.java
+++ User-Service/src/main/java/com/gachonproject/userservice/domain/user/entity/User.java
@@ -1,11 +1,10 @@
 package com.gachonproject.userservice.domain.user.entity;
 
-import com.gachonproject.userservice.domain.user.dto.UserCreateDto;
+import com.gachonproject.userservice.domain.user.dto.request.UserCreateDto;
 import com.gachonproject.userservice.global.common.entity.BaseEntity;
 import com.gachonproject.userservice.global.common.entity.enums.Role;
 import com.gachonproject.userservice.global.common.entity.enums.Sex;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,18 +17,25 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "student_id", unique = true)
     private String studentId;
 
+    @Column(name = "login_id", unique = true)
     private String loginId;
 
+    @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Enumerated(EnumType.STRING)
     private Sex sex;
 
     public static User of(UserCreateDto dto) {
