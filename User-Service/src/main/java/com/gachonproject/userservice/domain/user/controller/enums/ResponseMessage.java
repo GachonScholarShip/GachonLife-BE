ce01ad2eb1a7c180d743c3b70c1280dcd3a9deef
koreaioi <koreaioi@gachon.ac.kr>
2025-04-23 19:55:15 +0900

    feat: ResponseMessage 추가

--- User-Service/src/main/java/com/gachonproject/userservice/domain/user/controller/enums/ResponseMessage.java
+++ User-Service/src/main/java/com/gachonproject/userservice/domain/user/controller/enums/ResponseMessage.java
@@ -7,6 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    // UserController
+    USER_LIST_SUCCESS("사용자 목록을 반환합니다. "),
+    USER_UPDATE_SUCCESS("사용자 정보를 수정했습니다."),
+    USER_DELETE_SUCCESS("사용자를 삭제(회원탈퇴)했습니다."),
+
     // AuthController
     REGISTER_SUCCESS("회원가입에 성공했습니다.");
 
