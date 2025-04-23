5d4c5c92821649137d8f8b49598c433d2ad0421e
koreaioi <koreaioi@gachon.ac.kr>
2025-04-23 19:54:30 +0900

    feat: 사용자 목록 조회 UseCase 추가

---
+++ User-Service/src/main/java/com/gachonproject/userservice/domain/user/usecase/UserUseCase.java
@@ -0,0 +1,29 @@
+package com.gachonproject.userservice.domain.user.usecase;
+
+import com.gachonproject.userservice.domain.user.dto.request.UserUpdateDto;
+import com.gachonproject.userservice.domain.user.dto.response.UserResponseDto;
+import com.gachonproject.userservice.domain.user.entity.User;
+import com.gachonproject.userservice.domain.user.service.UserDeleteService;
+import com.gachonproject.userservice.domain.user.service.UserGetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserUseCase {
+
+    private final UserGetService userGetService;
+    private final UserDeleteService userDeleteService;
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> findUserList(int pageNumber, int pageSize) {
+        return userGetService.findUserList(pageNumber, pageSize)
+                .stream()
+                .map(UserResponseDto::from)
+                .toList();
+    }
+
+}
