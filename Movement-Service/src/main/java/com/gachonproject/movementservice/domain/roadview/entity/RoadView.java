72d67d02df4961cf57916524b4df875672e5df67
koreaioi <koreaioi@gachon.ac.kr>
2025-04-28 17:11:42 +0900

    feat: RoadView Entity 추가

---
+++ Movement-Service/src/main/java/com/gachonproject/movementservice/domain/roadview/entity/RoadView.java
@@ -0,0 +1,33 @@
+package com.gachonproject.movementservice.domain.roadview.entity;
+
+import com.gachonproject.movementservice.domain.roadview.dto.request.RoadViewSaveDto;
+import com.gachonproject.movementservice.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Table(name = "road_view")
+@SuperBuilder
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+public class RoadView extends BaseEntity {
+
+    @Column(nullable = false, length = 50)
+    String endPoint;
+
+    @Column(nullable = false, length = 512)
+    String url;
+
+    public static RoadView from(RoadViewSaveDto dto) {
+        return RoadView.builder()
+                .endPoint(dto.endPoint())
+                .url(dto.url())
+                .build();
+    }
+
+}
