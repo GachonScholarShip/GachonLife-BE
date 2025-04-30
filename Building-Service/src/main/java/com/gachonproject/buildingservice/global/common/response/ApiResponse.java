7e85ca698db4db410f779587d84ebefd712ddbf4
KilGwiJong <rlfrnlwhd@gmail.com>
2025-04-30 22:04:47 +0900

    api response

--- Building-Service/src/main/java/com/gachonproject/buildingservice/global/common/response/ApiResponse.java
+++ Building-Service/src/main/java/com/gachonproject/buildingservice/global/common/response/ApiResponse.java
@@ -5,22 +5,21 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
-
     private final int code;
     private final String message;
     private final T data;
 
-    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message, T data){
-        return new ApiResponse<>(httpStatus.value(), message, data);
-    }
-
-    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message){
-        return new ApiResponse<>(httpStatus.value(), message, null);
-    }
-
-    public ApiResponse(int code, String message, T data) {
-        this.code = code;
+    private ApiResponse(int code, String message, T data) {
+        this.code    = code;
         this.message = message;
-        this.data = data;
+        this.data    = data;
+    }
+
+    public static <T> ApiResponse<T> response(HttpStatus status, String message, T data) {
+        return new ApiResponse<>(status.value(), message, data);
+    }
+
+    public static <T> ApiResponse<T> response(HttpStatus status, String message) {
+        return new ApiResponse<>(status.value(), message, null);
     }
 }
