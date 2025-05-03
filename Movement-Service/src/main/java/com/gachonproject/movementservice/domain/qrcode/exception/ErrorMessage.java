0e4903a1db6938f3eab91eb4b87283dcd8765c7a
koreaioi <koreaioi@gachon.ac.kr>
2025-05-03 19:59:36 +0900

    feat: DuplicatedBuildingNameException 추가

---
+++ Movement-Service/src/main/java/com/gachonproject/movementservice/domain/qrcode/exception/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.gachonproject.movementservice.domain.qrcode.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorMessage {
+
+    DUPLICATED_BUILDING_NAME("해당 건물 QR코드는 이미 존재합니다.");
+
+    private final String message;
+}
+
