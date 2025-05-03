4a3bae89a635c9ee6b687ff2048408218894ac25
koreaioi <koreaioi@gachon.ac.kr>
2025-05-03 19:59:54 +0900

    feat: QrCode Entity 추가

---
+++ Movement-Service/src/main/java/com/gachonproject/movementservice/domain/qrcode/entity/QrCode.java
@@ -0,0 +1,32 @@
+package com.gachonproject.movementservice.domain.qrcode.entity;
+
+
+import com.gachonproject.movementservice.domain.qrcode.dto.request.QrCodeSaveDto;
+import com.gachonproject.movementservice.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QrCode extends BaseEntity {
+
+    @Column(nullable = false, length = 50)
+    private String buildingName;
+
+    @Column(nullable = false, length = 512)
+    private String imageUrl;
+
+    public static QrCode from(QrCodeSaveDto dto) {
+        return QrCode.builder()
+                .buildingName(dto.buildingName())
+                .imageUrl(dto.imageUrl())
+                .build();
+    }
+
+}
