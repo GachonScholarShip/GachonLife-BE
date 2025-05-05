e25b2aa2cb0398dc219223eabde7e400d9c53677
KilGwiJong <rlfrnlwhd@gmail.com>
2025-05-05 12:57:41 +0900

    feat: class entity 추가

---
+++ Building-Service/src/main/java/com/gachonproject/buildingservice/domain/classes/entity/Classes.java
@@ -0,0 +1,67 @@
+package com.gachonproject.buildingservice.domain.classes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "class")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class  Classes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+z
+    @Column(name = "title", nullable = false)
+    private String title;
+
+
+    @Column(name = "grade", nullable = false)
+    private String grade;
+
+
+    @Column(name = "major", nullable = false)
+    private String major;
+
+
+    @Column(name = "professor", nullable = false)
+    private String professor;
+
+
+    @Column(name = "course_time", nullable = false)
+    private String courseTime;
+
+
+    @Column(name = "timeslot", nullable = false)
+    private String timeSlot;
+
+
+    @Column(name = "building_name", nullable = false)
+    private String buildingName;
+
+
+    @Column(name = "room_name", nullable = false)
+    private String roomName;
+
+
+    @Column(name = "floor", nullable = false)
+    private Integer floor;
+
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
