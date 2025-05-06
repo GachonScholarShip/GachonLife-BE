5b98784c8b2cce8584bc1e2f427794378a0b2fb1
koreaioi <koreaioi@gachon.ac.kr>
2025-05-06 19:35:37 +0900

    fix: 반환 메세지 수정

--- Movement-Service/src/main/java/com/gachonproject/movementservice/domain/direction/controller/enums/SuccessMessage.java
+++ Movement-Service/src/main/java/com/gachonproject/movementservice/domain/direction/controller/enums/SuccessMessage.java
@@ -1,6 +1,5 @@
 package com.gachonproject.movementservice.domain.direction.controller.enums;
 
-import jakarta.annotation.security.DenyAll;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessMessage {
 
-    DIRECTION_DELETE_SUCCESS("길찾긱 정보를 삭제합니다."),
+    DIRECTION_DELETE_SUCCESS("길찾기 정보를 삭제합니다."),
     DIRECTION_UPDATE_SUCCESS("길찾기 정보를 수정합니다."),
     DIRECTION_LIST_GET_SUCCESS("길찾기 목록 정보를 반홥합니다."),
     DIRECTION_DETAIL_GET_SUCCESS("길찾기 단일 정보를 반환합니다."),
     DIRECTION_SAVE_SUCCESS("성공적으로 길찾기 정보를 저장했습니다.");
 
-    private final String messagge;
+    private final String message;
 
 }
